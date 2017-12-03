c627af26395227fcf9c63feba0e78b342baa7fb5
Lucas <lucas_mas@msn.com>
2017-12-04 00:41:34 +0100

    correction servo2.py

--- servo2.py
+++ servo2.py
@@ -5,12 +5,28 @@ import keyboard
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setwarnings(False)
+
+#Déclaration des variables
 p = GPIO.PWM(17,100)
 dutyCycle = 0
+i = 0
+
 p.start(dutyCycle)
 
-i = 0
+
+keyboard.hook(keydown)
+
+	
+	
 while i==0 :
+	p.ChangeDutyCycle(dutyCycle)
+	
+	
+p.stop()
+GPIO.cleanup()
+
+
+def keydown(e):
 	if keyboard.is_pressed('up') and dutyCycle <= 23:
 		dutyCycle = dutyCycle + 0.1
 
@@ -19,11 +35,6 @@ while i==0 :
 	
 	if keyboard.is_pressed('space') :
 		i = 1
-	
-	p.ChangeDutyCycle(dutyCycle)
-	
-p.stop()
-GPIO.cleanup()
 
 def angleToDutyCycle(a) :
 	return float(a)/10 + 5
