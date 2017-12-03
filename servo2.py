e9bb5b84e8a4bbee5551229c847342d9863e39f8
Lucas <lucas_mas@msn.com>
2017-12-04 00:20:25 +0100

    ajout servo2.py

---
+++ servo2.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+import keyboard
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+p = GPIO.PWM(17,100)
+dutyCycle = 0
+p.start(dutyCycle)
+
+i = 0
+while i=0 :
+	if keyboard.is_pressed('up') and dutyCycle <= 23:
+		dutyCycle = dutyCycle + 0.1
+
+	if keyboard.is_pressed('down') and dutyCycle >= 5 :
+		dutyCycle = dutyCycle - 0.1
+	
+	if keyboard.is_pressed('space') :
+		i = 1
+	
+	p.changeDutyCycle(dutyCycle)
+	
+p.stop()
+GPIO.cleanup()
+
+def angleToDutyCycle(a) :
+	return float(a)/10 + 5
